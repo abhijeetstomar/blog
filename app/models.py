46857a4388ca6466bd2f17cf12552e91f9a26777
abhijeetstomar <contact.abhijeetsingh@gmail.com>
2018-04-13 19:14:09 +0530

    Created SQLite data table for Post model using migrations

--- app/models.py
+++ app/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 # imports for Post model
 from django.utils import timezone
 from django.contrib.auth.models import User
@@ -10,16 +9,27 @@ class Post(models.Model):
         ('draft', 'Draft'),
         ('published', 'Published'),
     )
+    # Field for post title. CharField translates to a varchar column in the SQL database.
     title = models.CharField(max_length=250)
+    # This is a field intended to be used in URLs. A slug is a short label containing 
+    # only letters, numbers, underscores or hyphens.
     slug = models.SlugField(max_length=250, unique_for_date='publish')
-    author = models.ForeignKey(User, related_name='blog_posts')
+    # This field defines a many to one relationship. A user can write several posts.
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    # Body of the text. Translates to a text column in the sql database.
     body = models.TextField()
+    # This date time indicates when the post was published.
     publish = models.DateTimeField(default=timezone.now)
+    # This data time indicates when the post was created.
     created = models.DateTimeField(auto_now_add=True)
+    # This date time indicates the last time the post was updated.
     updated = models.DateTimeField(auto_now=True)
+    # Show the status of the post
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
+    # contains meta data
     class Meta:
+        # descending order is specified by the negative prefix
         ordering = ('-publish',)
 
     def __str__(self):
--- blog/settings.py
+++ blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -111,6 +112,7 @@ USE_I18N = True
 
 USE_L10N = True
 
+# Activate/ Deactivate timezone support
 USE_TZ = True
 
 
