184269aed60cbd353e2b89e0e31256282c05ef8e
abhijeetstomar <contact.abhijeetsingh@gmail.com>
2018-06-01 18:32:49 +0530

    Created list view

--- app/views.py
+++ app/views.py
@@ -1,34 +1,36 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-# Create your views here.
+from django.views.generic import ListView
+from .models import Post
 
-# request parameter is required by all views
-def post_list(request):
+
+def post_list(request, category=None):
     object_list = Post.published.all()
     paginator = Paginator(object_list, 3) # 3 posts in each page
-    # indicates current page number
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
-        # if page is not an integer deliver the first page
+        # If page is not an integer deliver the first page
         posts = paginator.page(1)
-
     except EmptyPage:
-        # if page is out of range deliver last page of results
+        # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
-    # render returns html code
     return render(request, 'app/post/list.html', {'page': page,
-                                                  'posts': posts})
+                                                   'posts': posts})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'app/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
-    # gives html 404 error if object is not found
-    post = get_object_or_404(Post, slug = post, 
-                                   status = 'published',
-                                   publish__year = year,
-                                   publish__month = month,
-                                   publish__day = day)
-
+    post = get_object_or_404(Post, slug=post,
+                                   status='published',
+                                   publish__year=year,
+                                   publish__month=month,
+                                   publish__day=day)
     return render(request, 'app/post/detail.html', {'post': post})
